56c146f93e6250879cddf4773a51b0afc7285ffc
nguyenmanhtien1209 <nguyenmanhtien12092002@gmail.com>
2026-07-26 19:44:42 +0700

    Test auto deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
 import axios from 'axios';
-// update: 26/07/2026
+// update: 26/07/2026 test code
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
