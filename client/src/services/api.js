eb7e5b3ddd7a3dfc2a8559babfaead993b5887e9
nguyenmanhtien1209 <nguyenmanhtien12092002@gmail.com>
2026-07-26 19:35:47 +0700

    Update api service

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
 import axios from 'axios';
-// update: 25/07/2026
+// update: 26/07/2026
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
